0c141e86e5e467f0642106fdaba0ae8bce846fdf
soyun9812 <soyun9812@naver.com>
2022-11-17 21:26:46 +0900

    20221117

---
+++ soyun/src/components/house/HouseFilter.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-row>
+        <h2>Filter</h2>
+    </v-row>
+</template>
+
+<script>
+export default {
+    name: "HouseFilter"
+}
+</script>
+
+<style>
+
+</style>
---
+++ soyun/src/components/layout/HouseSearch.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>HouseSearch</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HouseSearch"
+}
+</script>
+
+<style>
+
+</style>
