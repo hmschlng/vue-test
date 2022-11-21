81270cfaa3fbe4ddb8e7872709d44114ae9d42e1
JoEunSeo <esmint@kumoh.ac.kr>
2022-11-21 17:51:16 +0900

    20221121

--- soyun/.history/src/views/AppMember_20221121173120.vue
+++ soyun/.history/src/views/AppMember_20221121173120.vue
@@ -1,6 +1,5 @@
 <template>
-  <v-main>
-    <video muted autoplay loop>
+  <video muted autoplay loop>
       <source src="@/assets/main.mp4" type="video/mp4" />
     </video>
     <v-overlay z-index="0">
@@ -11,16 +10,9 @@
 
 <script>
 export default {
-  name: "MemberLogin",
+  name: "AppMember",
 };
 </script>
 
 <style scoped>
-video {
-  position: fixed;
-  object-fit: fill;
-}
-html {
-  overflow: hidden !important;
-}
 </style>
